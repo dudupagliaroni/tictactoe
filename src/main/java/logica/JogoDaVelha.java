077b5b9154d642f07c403d6f0baab47b7babb4bf
Eduardo Pagliaroni <dudupagliaroni@gmail.com>
2020-11-04 22:44:44 -0300

    update

--- src/main/java/logica/JogoDaVelha.java
+++ src/main/java/logica/JogoDaVelha.java
@@ -2,36 +2,50 @@ package logica;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.prefs.Preferences;
 
-public class JogoDaVelha extends JPanel implements MouseListener {
-    Font minhaFont = new Font("Arial", Font.BOLD, 120);
-    Font minhaFontPeq = new Font("Arial", Font.BOLD, 40);
+public class JogoDaVelha extends JPanel implements MouseListener, KeyListener {
+    Font minhaFontGrande = new Font("Arial", Font.BOLD, 120);
+    Font minhaFontMedia = new Font("Arial", Font.BOLD, 40);
+    Font minhaFontPeq = new Font("Arial", Font.BOLD, 20);
+
     int matriz[][];
     static int jogador = 1;
     int ganhador = 0;
+    private boolean jogarNov = false;
+
+    private int vitoriaP1 =0;
+    private int vitoriaP2 = 0;
+    private int empates = 0;
 
     public JogoDaVelha() {
         matriz = new int[3][3];
+        imprimirTabuleiro();
 
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                System.out.print(matriz[i][j]);
-            }
-            System.out.println();
-        }
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setFont(minhaFont);
+    protected void paintComponent(Graphics g2) {
+        Graphics2D g = (Graphics2D) g2.create();
+
+        if (jogarNov) {
+            int jogarNovamente = new JOptionPane().showConfirmDialog(this, "Deseja jogar novamente?");
+            if (jogarNovamente == JOptionPane.OK_OPTION) {
+                jogarNov = false;
+                reiniciarJogo();
+            } else {
+                System.exit(1);
+            }
+        }
+        g.setFont(minhaFontGrande);
 
         g.setColor(Color.white);
         g.fillRect(0, 0, 600, 600);
 
+        g.setStroke(new BasicStroke(5));
         g.setColor(Color.black);
         g.drawLine(0, 200, 600, 200);
         g.drawLine(0, 400, 600, 400);
@@ -51,11 +65,34 @@ public class JogoDaVelha extends JPanel implements MouseListener {
 
         if (ganhador !=0){
             g.setColor(Color.red);
-            g.setFont(minhaFontPeq);
+            g.setFont(minhaFontMedia);
             g.drawString("O jogador " + ganhador + " venceu!!!", 100,190);
-
+            jogarNov = true;
+            repaint();
         }
 
+
+
+    }
+
+    private void reiniciarJogo() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = 0;
+                ganhador = 0;
+            }
+
+        }
+    }
+
+    private void imprimirTabuleiro() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println();
+
+        }
     }
 
 
@@ -78,15 +115,8 @@ public class JogoDaVelha extends JPanel implements MouseListener {
         }
 
         verificarGanhador();
-
         repaint();
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                System.out.print(matriz[i][j]);
-            }
-            System.out.println();
-        }
+        imprimirTabuleiro();
 
     }
 
@@ -97,7 +127,7 @@ public class JogoDaVelha extends JPanel implements MouseListener {
                 System.out.println("Houve um ganhador linha");
                 ganhador = matriz[i][0];
                 System.out.println("Ganhador é " + ganhador);
-                break;
+
             }
         }
 
@@ -116,6 +146,8 @@ public class JogoDaVelha extends JPanel implements MouseListener {
                 ganhador = matriz[1][1];
                 System.out.println("Ganhador é " + ganhador);
             }
+            if (ganhador == 1) vitoriaP1++;
+            if (ganhador == 2) vitoriaP2++;
 
         }
 
@@ -137,6 +169,21 @@ public class JogoDaVelha extends JPanel implements MouseListener {
     }
 
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
